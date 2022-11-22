916682ac67081cc7a9b642d17b810189a271457a
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2022-11-22 13:01:48 -0500

    Make board printable

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::collections::HashMap;
 
 use anyhow::{anyhow, Result};
 
-fn main() {
-    let game = Game::new(3).unwrap();
-    println!("{:?}", game);
+fn main() -> Result<()> {
+    let game = Game::new(3)?;
+    println!("{}", game.render_board());
+    Ok(())
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -47,6 +48,7 @@ struct Player {
 struct Game<'a> {
     pub players: Vec<Player>,
     board_state: BoardState<'a>,
+    grid_dimensions: u8,
     turn_number: u32,
     winner: Option<&'a Player>,
 }
@@ -56,6 +58,47 @@ impl Game<'_> {
     // NOTE: be sure to also add more entries to COLUMN_HEADERS if increasing max num cols
     const MAX_NUM_ROWS_OR_COLUMNS: u8 = 8;
     const COLUMN_HEADERS: [char; 8] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+    fn get_tile_from_indices(&self, row_index: usize, column_index: usize) -> Option<&Tile> {
+        let row = row_index as u8 + 1;
+        let column = Self::COLUMN_HEADERS[column_index];
+        let coords = Coordinates { row, column };
+        self.board_state.tiles.get(&coords)
+    }
+
+    fn render_board(&self) -> String {
+        let mut rendered_grid = String::new();
+        // NOTE: we operate on the assumption that the board is a square -- its
+        // number of rows and columns are equal, and every one of them contains
+        // the same number of items
+        for row_index in 0..self.grid_dimensions as usize {
+            let mut rendered_row = String::new();
+            for column_index in 0..self.grid_dimensions as usize {
+                let tile = self.get_tile_from_indices(row_index, column_index).unwrap();
+                use TileOccupationState::*;
+                let tile_mark = match tile.occupation_state {
+                    Empty => ' ',
+                    Occupied(player) => player.mark,
+                };
+                let cell_ending = if column_index + 1 < self.grid_dimensions as usize {
+                    '|'
+                } else {
+                    '\n'
+                };
+                let cell = &format!(" {} {}", tile_mark, cell_ending);
+                rendered_row.push_str(cell);
+            }
+            rendered_grid.push_str(&rendered_row);
+            // if the row we just added wasn't the last one...
+            if row_index + 1 < self.grid_dimensions as usize {
+                // ... then build and add a divider row. We subtract 1 from the length to account
+                // for the newline at the end.
+                let divider_row = format!("{}\n", "-".repeat(rendered_row.len() - 1));
+                rendered_grid.push_str(&divider_row);
+            }
+        }
+        rendered_grid
+    }
 
     fn new(num_rows_or_columns: u8) -> Result<Self> {
         if num_rows_or_columns < Self::MIN_NUM_ROWS_OR_COLUMNS
@@ -96,6 +139,7 @@ impl Game<'_> {
         Ok(Self {
             players,
             board_state: BoardState { tiles },
+            grid_dimensions: num_rows_or_columns,
             turn_number: 1,
             winner: None,
         })
@@ -104,10 +148,26 @@ impl Game<'_> {
 
 // add helper fn for getting current turn player (use turn number -1 to index into players vec,
 //   protecting against overflow from zero to neg 1)
+// add a `render_turn`
+// accept user input for coords
+// actually update board state every turn
+// actually check for victory at the end of every turn, print winner, stop game
+
+// be very friendly in stripping whitespace and accepting lowercase chars for coords
 // use official turn-building logic even during init?
 // add ability to render a game board, incl coords
 // refactor out to multiple modules/files
 // refactor to smaller functions, esp with more `new()` functions
 // add test coverage
 // remove many Debug derives
+// can I clean up 'cell' logic using map + join, so I join with '|'?
+// try to always use some kind of safe cast and index lookup
+// sprinkle in `+` in rows to match up with vertical lines
+// print row and column headers
+// maybe clean up logic for how we look up tiles -- by indices always? by 'coords' always?
+// maybe clean up how we build coords, so it's more foolproof about adding 1 to convert 0-indexed to 1
+// allow min_rows_cols all the way down to 1?
 // is it possible to fully avoid use of unwrap?
+// somehow make get_tile_from_indices less dangerous, by letting it have named parmams via some kind of struct?
+// generally do less casting
+// can I make COLUMN_HEADERS a vec and fill it up automatically?
